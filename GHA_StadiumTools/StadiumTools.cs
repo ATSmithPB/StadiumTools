0897e1938f6a52701ab98e38d8c876b56acca526
ATSmithPB <97251392+ATSmithPB@users.noreply.github.com>
2022-06-29 23:53:31 +0200

    Add files via upload
    revised access modifiers and started building section geometry construction method

--- GHA_StadiumTools/StadiumTools.cs
+++ GHA_StadiumTools/StadiumTools.cs
@@ -3,7 +3,7 @@
 namespace StadiumTools
 {
     /// <summary>
-    /// This object stores a collection of unit conversion coeffecients to handle various modeling unit spaces
+    /// Stores a collection of unit conversion coeffecients to handle various modeling unit spaces
     /// </summary>
     public struct UnitHandler
     {
@@ -16,10 +16,11 @@ namespace StadiumTools
     }
 
     /// <summary>
-    /// A point in 2D space (h,v)
+    /// Represents a point in 2D space (h,v)
     /// </summary>
     public struct Pt2d
     {
+        //Properties
         /// <summary>
         /// Horizontal distance from origin
         /// </summary>
@@ -28,13 +29,23 @@ namespace StadiumTools
         /// Vertical distance from origin
         /// </summary>
         public double V { get; set; }
+
+        //Constructors
+        public Pt2d(double h, double v)
+        {
+            this.H = h;
+            this.V = v;
+        }
+
+        //Methods
     }
 
     /// <summary>
-    /// A vector in 2D space (h,v)
+    /// Represents a vector in 2D space (h,v)
     /// </summary>
     public struct Vec2d
     {
+        //Properties
         /// <summary>
         /// Horizontal component of vector.
         /// </summary>
@@ -47,10 +58,20 @@ namespace StadiumTools
         /// Length of vector
         /// </summary>
         public double L { get; set; }
+
+        //Constructors
+        public Vec2d(double h, double v, double l)
+        {
+            this.H = h;
+            this.V = v;
+            this.L = l;
+        }
+
+        //Methods
     }
 
     /// <summary>
-    /// A point in 3D space (x, y, z)
+    /// Represents a point in 3D space (x, y, z)
     /// </summary>
     public struct Pt3d
     {
@@ -60,19 +81,23 @@ namespace StadiumTools
     }
 
     /// <summary>
-    /// This object contains the properties of a Spectator. 
+    /// Represents the properties of a single Spectator. 
     /// </summary>
-    internal class Spectator
+    public class Spectator
     {
         //Properties
         /// <summary>
-        /// a tuple represneting the 2d (h(x), v(y)) coordinates of the spectator relative to the P.O.F (Point of focus)
+        /// a point representing the location of the spectator in 2d space relative to the P.O.F
+        /// </summary>
+        public Pt2d loc2d { get; set; }
+        /// <summary>
+        /// a point represneting the location of the P.O.F (Point of focus)
         /// </summary>
         public Pt2d pof { get; set; }
         /// <summary>
         /// a tuple representing the 2d (h(x), v(y)) components and Length of a spectators sightline to the P.O.F
         /// </summary>
-        public (double H, double V, double Length) sightLine { get; set; }
+        public Vec2d sightLine { get; set; }
         /// <summary>
         /// True if the spectator has an unobstructed sightline to the P.O.F
         /// </summary>
@@ -92,95 +117,121 @@ namespace StadiumTools
         }
 
         //Methods
+
+        /// <summary>
+        /// Initializes a spectator with default values.
+        /// </summary>
         public void Initialize()
         {
-            this.loc2D = (0.0, 0.0);
-            this.sightLine = (0.0, 0.0, 0.0);
+            this.loc2d = new Pt2d();
+            this.pof = new Pt2d();
+            this.sightLine = new Vec2d();
             this.hasSightLine = false;
             this.cVal = 0.0;
         }
     }
 
     /// <summary>
-    /// This object contains the data of a single seating tier.
+    /// Represents the data of a single seating tier.
     /// </summary>
-    internal class Tier
+    public class Tier
     {
+        //Enums
+        /// <summary>
+        /// Available methods to generate a reference point for a tier
+        /// </summary>
+        public enum RefPtType
+        {
+            ByPOF,
+            ByEndOfPrevTier
+        }
+
         //Properties
         /// <summary>
-        /// Model unit space of the tier (mm, m, in, ft) 
+        /// Method for determining the reference point of the tier
         /// </summary>
-        public double unit;
+        public RefPtType RefPt { get; set; }
         /// <summary>
-        /// Horizontal offset of Tier Start from Point of Focus (P.O.F)
+        /// Coeffecient for model unit space of the tier (mm, m, in, ft) 
         /// </summary>
-        public double startH { get; set; }
+        public double Unit;
         /// <summary>
-        /// Vertical offset of tier start from Point of Focus (P.O.F)
+        /// Point of focus for the spectators of this tier 
         /// </summary>
-        public double startV { get; set; }
+        public Pt2d POF { get; set; }
+        /// <summary>
+        /// Horizontal offset of Tier Start from reference point
+        /// </summary>
+        public double StartH { get; set; }
+        /// <summary>
+        /// Vertical offset of tier start from reference point
+        /// </summary>
+        public double StartV { get; set; }
         /// <summary>
         /// Maximum allowable c-value for spectators in this tier.
         /// </summary>
-        public double cValue { get; set; }
+        public double Cvalue { get; set; }
         /// <summary>
         /// Horizontal offset of seated spectator eyes from riser nose 
         /// </summary>
-        public double eyeH { get; set; }
+        public double EyeH { get; set; }
         /// <summary>
         /// Vertical offset of seated spectator eyes from floor
         /// </summary>
-        public double eyeV { get; set; }
+        public double EyeV { get; set; }
         /// <summary>
         /// Horizontal offset of standing spectator eyes from riser nose 
         /// </summary>
-        public double sEyeH { get; set; }
+        public double SEyeH { get; set; }
         /// <summary>
         /// Vertical offset of seated spectator eyes from floor
         /// </summary>
-        public double sEyeV { get; set; }
+        public double SEyeV { get; set; }
         /// <summary>
         /// Number of rows in this tier not including super risers
         /// </summary>
-        public int rowCount { get; set; }
+        public int RowCount { get; set; }
         /// <summary>
         /// True if tier contains a vomitory
         /// </summary>
-        public bool vomHas { get; set; }
+        public bool VomHas { get; set; }
         /// <summary>
         /// Row number of vomitory start
         /// </summary>
-        public int vomStart { get; set; }
+        public int VomStart { get; set; }
         /// <summary>
         /// Height of vomitory in rows 
         /// </summary>
-        public int vomHeight { get; set; }
+        public int VomHeight { get; set; }
         /// <summary>
         /// Vertical height of fascia below the first row.
         /// </summary>
-        public double fasciaH { get; set; }
+        public double FasciaH { get; set; }
         /// <summary>
         /// True if tier has a super riser
         /// </summary>
-        public bool hasSuper { get; set; }
+        public bool HasSuper { get; set; }
         /// <summary>
         /// Start row for inserting super riser
         /// </summary>
-        public int superStart { get; set; }
+        public int SuperStart { get; set; }
         /// <summary>
         /// Optional chamfer distance for nose of super riser
         /// </summary>
-        public double superChamfer { get; set; }
+        public double SuperChamfer { get; set; }
         /// <summary>
         /// Horizontal offset of spectator eyes from nose of super riser 
         /// </summary>
-        public double superEyeH { get; set; }
+        public double SuperEyeH { get; set; }
         /// <summary>
         /// Vertical offset of spectator eyes from floor of super riser
         /// </summary>
-        public double superEyeV { get; set; }
+        public double SuperEyeV { get; set; }
        
         //Constructors
+        /// <summary>
+        /// Initializes a new default Tier
+        /// </summary>
         public Tier()
         {
             Initialize();
@@ -192,38 +243,78 @@ namespace StadiumTools
         /// </summary>
         public void Initialize()
         {
-            this.unit = UnitHandler.m;
-            this.startH = 2.0 * unit;
-            this.startV = 5 * unit;
-            this.cValue = 0.10 * unit;
-            this.eyeH = 0.8 * unit;
-            this.eyeV = 1.2 * unit;
-            this.sEyeH = 0.8 * unit;
-            this.sEyeV = 2.5 * unit;
-            this.rowCount = 20;
-            this.vomHas = true;
-            this.vomStart = 5;
-            this.vomHeight = 5;
-            this.fasciaH = 1.0 * unit;
-            this.hasSuper = true;
-            this.superStart = 15;
-            this.superChamfer = 0.01 * unit;
-            this.superEyeH = 0.8 * unit;
-            this.superEyeV = 2.5 * unit;
+            this.Unit = UnitHandler.m;
+            this.RefPt = RefPtType.ByPOF;
+            this.StartH = 2.0 * Unit;
+            this.StartV = 5 * Unit;
+            this.Cvalue = 0.10 * Unit;
+            this.EyeH = 0.8 * Unit;
+            this.EyeV = 1.2 * Unit;
+            this.SEyeH = 0.8 * Unit;
+            this.SEyeV = 2.5 * Unit;
+            this.RowCount = 20;
+            this.VomHas = true;
+            this.VomStart = 5;
+            this.VomHeight = 5;
+            this.FasciaH = 1.0 * Unit;
+            this.HasSuper = true;
+            this.SuperStart = 15;
+            this.SuperChamfer = 0.01 * Unit;
+            this.SuperEyeH = 0.8 * Unit;
+            this.SuperEyeV = 2.5 * Unit;
         }
     }
 
-
-    internal class Tier2d
+    /// <summary>
+    /// Represents an ordered collection of Tier objects that exist in the same plane
+    /// </summary>
+    public class Section
     {
+        //Properties
         /// <summary>
-        /// Tier to inherit geometric properties from
+        /// An ordered list of tiers to comprise the section.
         /// </summary>
-        public Tier tier { get; set; }
+        public Tier[] Tiers { get; set; }
         /// <summary>
-        /// 2d coordinate representing the point-of-focus of the seating tier
+        /// A list of 2d points that represent the outline of a seating tier
         /// </summary>
-        public (double h, double v) pof { get; set; }
-        public List<()>
+        public Pt2d POF { get; set; }
+
+        //Constructors
+        /// <summary>
+        /// Construct a Section from a list of tiers 
+        /// </summary>
+        /// <param name="tiers"></param>
+        public Section(List<Tier> tierList)
+        {
+            Tier[] tiers = tierList.ToArray();
+            this.Tiers = tiers;
+            this.POF = new Pt2d(0.0, 0.0);
+        }
+
+        /// <summary>
+        /// Construct a Section from an array of tiers
+        /// </summary>
+        /// <param name="tiers"></param>
+        public Section(Tier[] tiers)
+        {
+            this.Tiers = tiers;
+            this.POF = new Pt2d(0.0, 0.0);
+        }
+
+        /// <summary>
+        /// Construct an empty Section from n tiers 
+        /// </summary>
+        /// <param name="nTiers"></param>
+        public Section(int nTiers)
+        {
+            Tier[] tiers = new Tier[nTiers];
+            this.Tiers = tiers;
+            this.POF = new Pt2d(0.0, 0.0);
+        }
+
+        //Methods
+      
+
     }
 }
