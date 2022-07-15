ca979d52651701001cf7a718c8be58b2cb05919e
ATSmithPB <97251392+ATSmithPB@users.noreply.github.com>
2022-07-15 14:18:41 +0200

    change basis transform matrix troubleshooting

--- StadiumTools/IO_Rhino.cs
+++ StadiumTools/IO_Rhino.cs
@@ -20,8 +20,25 @@ namespace StadiumTools
 
             public static Pln3d Pln3dFromPlane(Plane plane)
             {
-                Pln3d pln = new Pln3d(plane.Origin, plane.XAxis, plane.YAxis, plane.ZAxis)
+               Pt3d pOrigin = Pt3dFromPoint3d(plane.Origin);
+               Vec3d xAxis = Vec3dFromVector3d(plane.XAxis);
+               Vec3d yAxis = Vec3dFromVector3d(plane.YAxis);
+               Vec3d zAxis = Vec3dFromVector3d(plane.ZAxis);
+
+                Pln3d pln = new Pln3d(pOrigin, xAxis, yAxis, zAxis);
                 return pln;
+            }
+
+            public static Pt3d Pt3dFromPoint3d(Point3d point3d)
+            {
+                Pt3d pt = new Pt3d(point3d.X, point3d.Y, point3d.Z);
+                return pt;
+            }
+
+            public static Vec3d Vec3dFromVector3d(Vector3d vector3d)
+            {
+                Vec3d vec = new Vec3d(vector3d.X, vector3d.Y, vector3d.Z);
+                return vec;
             }
         }
     }
--- StadiumTools/Transform3d.cs
+++ StadiumTools/Transform3d.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Numerics;
+using static System.Math;
 
 namespace StadiumTools
 {
@@ -120,11 +121,10 @@ namespace StadiumTools
 
             //Empty transform to become Change Basis
             Transform3d CB = new Transform3d();
-            CB.ChangeBasis()
+            success = CB.ChangeBasis(Vec3d.XAxis, Vec3d.YAxis, Vec3d.ZAxis, X1, Y1, Z1);
 
             Transform3d result = CB * T1 * F0;
 
-            success = true;
             return success;
         }
 
@@ -137,13 +137,153 @@ namespace StadiumTools
             double b = X1 * Z1;
             double c = Y1 * Z1;
 
-            double[][] jaggedArray3 =
+            double[][] R =
             {
-                new double[] { X1*X1, 3, 5, 7, 9 },
-                new double[] { 0, 2, 4, 6 },
-                new double[] { 11, 22 },
-                new double[] { 0, 1, 3, 5 }
+                new double[] { X1*X1, a, b,X1*X0, X1*Z0 },
+                new double[] { a, Y1*Y1, c, Y1*X0, Y1*Y0, Y1*Z0 },
+                new double[] { b, c, Z1*Z1, Z1*X0, Z1*Y0, Z1*Z0 }
             };
+
+            int i0 = (R[0][0] >= R[1][1]) ? 0 : 1;
+            
+            if (R[2][2] > R[i0][i0])
+            {
+                i0 = 2;
+            }
+
+            int i1 = (i0 + 1) % 3;
+            int i2 = (i1 + 1) % 3;
+
+            if (R[i0][i0] == 0.0)
+            {
+                return false;
+            }
+
+            double d = 1.0 / R[i0][i0];
+            R[i0][0] *= d;
+            R[i0][1] *= d;
+            R[i0][2] *= d;
+            R[i0][3] *= d;
+            R[i0][4] *= d;
+            R[i0][5] *= d;
+            R[i0][i0] = 1.0;
+            
+            if (R[i1][i0] != 0.0)
+            {
+                d = -R[i1][i0];
+                R[i1][0] += d * R[i0][0];
+                R[i1][1] += d * R[i0][1];
+                R[i1][2] += d * R[i0][2];
+                R[i1][3] += d * R[i0][3];
+                R[i1][4] += d * R[i0][4];
+                R[i1][5] += d * R[i0][5];
+                R[i1][i0] = 0.0;
+            }
+            
+            if (R[i2][i0] != 0.0)
+            {
+                d = -R[i2][i0];
+                R[i2][0] += d * R[i0][0];
+                R[i2][1] += d * R[i0][1];
+                R[i2][2] += d * R[i0][2];
+                R[i2][3] += d * R[i0][3];
+                R[i2][4] += d * R[i0][4];
+                R[i2][5] += d * R[i0][5];
+                R[i2][i0] = 0.0;
+            }
+
+            if (Abs((R[i1][i1])) < Abs((R[i2][i2])))
+            {
+                int i = i1; i1 = i2; i2 = i;
+            }
+
+            if (R[i1][i1] == 0.0)
+            {
+                return false;
+            }
+
+            d = 1.0 / R[i1][i1];
+            R[i1][0] *= d;
+            R[i1][1] *= d;
+            R[i1][2] *= d;
+            R[i1][3] *= d;
+            R[i1][4] *= d;
+            R[i1][5] *= d;
+            R[i1][i1] = 1.0;
+            
+            if (R[i0][i1] != 0.0)
+            {
+                d = -R[i0][i1];
+                R[i0][0] += d * R[i1][0];
+                R[i0][1] += d * R[i1][1];
+                R[i0][2] += d * R[i1][2];
+                R[i0][3] += d * R[i1][3];
+                R[i0][4] += d * R[i1][4];
+                R[i0][5] += d * R[i1][5];
+                R[i0][i1] = 0.0;
+            }
+            
+            if (R[i2][i1] != 0.0)
+            {
+                d = -R[i2][i1];
+                R[i2][0] += d * R[i1][0];
+                R[i2][1] += d * R[i1][1];
+                R[i2][2] += d * R[i1][2];
+                R[i2][3] += d * R[i1][3];
+                R[i2][4] += d * R[i1][4];
+                R[i2][5] += d * R[i1][5];
+                R[i2][i1] = 0.0;
+            }
+
+            if (R[i2][i2] == 0.0)
+            {
+                return false;
+            }
+
+            d = 1.0 / R[i2][i2];
+            R[i2][0] *= d;
+            R[i2][1] *= d;
+            R[i2][2] *= d;
+            R[i2][3] *= d;
+            R[i2][4] *= d;
+            R[i2][5] *= d;
+            R[i2][i2] = 1.0;
+            
+            if (R[i0][i2] != 0.0)
+            {
+                d = -R[i0][i2];
+                R[i0][0] += d * R[i2][0];
+                R[i0][1] += d * R[i2][1];
+                R[i0][2] += d * R[i2][2];
+                R[i0][3] += d * R[i2][3];
+                R[i0][4] += d * R[i2][4];
+                R[i0][5] += d * R[i2][5];
+                R[i0][i2] = 0.0;
+            }
+            
+            if (R[i1][i2] != 0.0)
+            {
+                d = -R[i1][i2];
+                R[i1][0] += d * R[i2][0];
+                R[i1][1] += d * R[i2][1];
+                R[i1][2] += d * R[i2][2];
+                R[i1][3] += d * R[i2][3];
+                R[i1][4] += d * R[i2][4];
+                R[i1][5] += d * R[i2][5];
+                R[i1][i2] = 0.0;
+            }
+
+            TF.M[0][0] = R[0][3];
+            TF.M[0][1] = R[0][4];
+            TF.M[0][2] = R[0][5];
+            TF.M[1][0] = R[1][3];
+            TF.M[1][1] = R[1][4];
+            TF.M[1][2] = R[1][5];
+            TF.M[2][0] = R[2][3];
+            TF.M[2][1] = R[2][4];
+            TF.M[2][2] = R[2][5];
+
+            return true;
 
         }
         
